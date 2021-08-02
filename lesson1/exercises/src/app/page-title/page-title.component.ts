4e270ba57aab6973cf2e0e9848ab256417c4b8df
Paul Thompson <paul.t.thompson83@gmail.com>
2021-08-02 18:39:47 -0500

    Finished exercises.

--- lesson1/exercises/src/app/page-title/page-title.component.ts
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-title.component.css']
 })
 export class PageTitleComponent implements OnInit {
-
+  title = "Super cool awesome page!"
   constructor() { }
 
   ngOnInit() {
